d093e40f868cf81fc98118cb65a58bf68f612285
Софи Скорогудаева <kiss_wiwa_2003@mail.ru>
2022-02-17 00:21:43 +0300

    начата работа над панелью параметров. функционал начнется от изменения цвета для дальнейшего тестирования макета

---
+++ DocTemplate/ViewModel/ControlPanels/Settings/SettingsVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocTemplate.ViewModel.ControlPanels.Settings
+{
+    public class SettingsVm
+    {
+    }
+}
---
+++ DocTemplate/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using DocTemplate.Helpers;
+using DocTemplate.ViewModel.ControlPanels.Settings;
+
+namespace DocTemplate.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        #region Команды
+        public RelayCommand SettingsCommand { get; set; }
+
+        #endregion
+
+        #region Верски
+        public SettingsVm SettingsVm { get; set; }
+
+        #endregion
+
+        private bool _nothingSelected;
+        public bool NothingSelected
+        {
+            get { return _nothingSelected; }
+            set
+            {
+                _nothingSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            NothingSelected = true;
+
+            SettingsVm = new SettingsVm();
+
+            SettingsCommand = new RelayCommand(o => {ChangeCurrentView(SettingsVm);});
+        }
+
+        private void ChangeCurrentView(object viewToChange)
+        {
+            CurrentView = viewToChange;
+            NothingSelected = false;
+        }
+    }
+}
