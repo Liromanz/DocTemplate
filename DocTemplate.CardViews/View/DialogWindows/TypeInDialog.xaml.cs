0a443c99b120bbcb76e6f6931e0f9578de965fdd
Софи Скорогудаева <kiss_wiwa_2003@mail.ru>
2022-03-07 15:16:27 +0300

    созданы диалоговые окна и стили для них. создан файл для моих шаблонов

---
+++ DocTemplate.CardViews/View/DialogWindows/TypeInDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace DocTemplate.CardViews.View.DialogWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для TypeInDialog.xaml
+    /// </summary>
+    public partial class TypeInDialog : Window
+    {
+        public TypeInDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseDialog(object sender, RoutedEventArgs e) => Close();
+    }
+}
---
+++ DocTemplate.CardViews/View/DialogWindows/YesNoDialog.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace DocTemplate.CardViews.View.DialogWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для YesNoDialog.xaml
+    /// </summary>
+    public partial class YesNoDialog : Window
+    {
+        public YesNoDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseDialog(object sender, RoutedEventArgs e) => Close();
+    }
+}
