e77310e52bf7bbc433e3c20697e00e8d25b736c4
Софи Скорогудаева <kiss_wiwa_2003@mail.ru>
2022-03-10 17:51:30 +0300

    сверстана страница создания шаблона

---
+++ DocTemplate.CardViews/View/BackButton.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DocTemplate.CardViews.View
+{
+    /// <summary>
+    /// Логика взаимодействия для BackButton.xaml
+    /// </summary>
+    public partial class BackButton : UserControl
+    {
+        public BackButton()
+        {
+            InitializeComponent();
+        }
+
+        public ICommand ButtonCommand
+        {
+            get => (ICommand)GetValue(ButtonCommandProperty);
+            set => SetValue(ButtonCommandProperty, value);
+        }
+        // Using a DependencyProperty as the backing store for ThemeName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ButtonCommandProperty =
+            DependencyProperty.Register("ButtonCommand", typeof(ICommand), typeof(BackButton), new UIPropertyMetadata(null));
+    }
+}
---
+++ DocTemplate/View/TemplateCreatorWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace DocTemplate.View
+{
+    /// <summary>
+    /// Логика взаимодействия для TemplateCreatorWindow.xaml
+    /// </summary>
+    public partial class TemplateCreatorWindow : Window
+    {
+        public TemplateCreatorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
