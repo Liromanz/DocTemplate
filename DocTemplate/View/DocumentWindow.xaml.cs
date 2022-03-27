c0138523acb783a1ef3e53385b384d2a693f9b67
Софи Скорогудаева <kiss_wiwa_2003@mail.ru>
2022-03-27 19:19:06 +0300

    сверстана страница редактирования документа

---
+++ DocTemplate/View/DocumentWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DocTemplate.View
+{
+    /// <summary>
+    /// Логика взаимодействия для DocumentWindow.xaml
+    /// </summary>
+    public partial class DocumentWindow : Window
+    {
+        public DocumentWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
